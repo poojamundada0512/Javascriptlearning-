b60efdd1c06b5b2ab510ce0d605cec60f406ee95
poojamundada0512 <mundadapooja0512@gmail.com>
2023-10-06 16:27:19 +0530

    JavaScriptLearning and practice

---
+++ var.js
@@ -0,0 +1,11 @@
+"use strict";
+//Variables
+//decalre a variables
+var firstname="pooja";
+
+//use a variable
+console.log(firstname);
+
+//change the value
+firstname="mundada";
+console.log(firstname); 
