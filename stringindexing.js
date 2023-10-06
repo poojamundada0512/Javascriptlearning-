b5a12f39685604298cc4428581c715e763a33fa8
poojamundada0512 <mundadapooja0512@gmail.com>
2023-10-06 18:07:17 +0530

    Stringindexing file

---
+++ stringindexing.js
@@ -0,0 +1,22 @@
+//String Indexing
+
+let firstName = "pooja";
+
+// p o o j a 
+// 0 1 2 3 4
+
+console.log(firstName[3]); //index starts from 0
+
+
+//length of string
+//firstName.length
+console.log(firstName.length); //this will help you to get know about string length also if you add spaces it will count the spaces too
+   
+
+//last Index  : length-1
+
+console.log(firstName[firstName.length-1]); //this will give you the last letter of string 
+console.log(firstName[firstName.length-2]); //this will give you the second last letter of string
+
+
+//This string indexing will always count the spacing.
